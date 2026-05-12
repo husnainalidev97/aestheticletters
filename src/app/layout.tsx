2bc987e4365e62117ff61c522e29ea144fa7d5fd
Husnain Ali <husnainalidev97@gmail.com>
2026-05-12 19:31:06 +0300

    updated clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -110,7 +110,7 @@ export default function RootLayout({
 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "wmzskghpwc");`}
+})(window, document, "clarity", "script", "wnvsu8cqo6");`}
         </Script>
         {/* Google Analytics */}
         <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
